0f3192315003611bf53073c383c3baaef530c58a
Brenno Daniel <brenno.daniel.98@gmail.com>
2020-07-21 21:49:31 -0300

    update code example file 2-objetoRevisao.js

--- web-moderno-JS-udemy/4 - objeto/2-objetoRevisao.js
+++ web-moderno-JS-udemy/4 - objeto/2-objetoRevisao.js
@@ -5,10 +5,14 @@ produto['marca do produto'] = 'Generica'
 produto.preco = 220
 
 console.log(produto)
+
+// Podemos excluir um atributo de um objeto através do delete
 delete produto.preco
 delete produto['marca do produto']
 console.log(produto)
 
+// Podemos ter objetos dentro de objetos, e arrays também, com seus próprios atributos
+// Criação literal de objetos
 const carro = {
     modelo: 'A4',
     valor: 89000,
@@ -27,18 +31,25 @@ const carro = {
         nome: 'Ana',
         idade: 26
     }],
+    // Podemos ter funções também
     calcularValorSeguro: function() {
         // ...
     }
 }
 
+// Podemos acessar o atributo a partir da notação ponto, 
+// utilizamos essa forma quando já sabemos quais são os atributos do objeto
 carro.proprietario.endereco.numero = 1000
+
+// Podemos acessar o atributo também a partir da notação colchetes, a partir de strings
 carro['proprietario']['endereco']['logradouro'] = 'Av Gigante'
 console.log(carro)
 
+// Podemos deletar um objeto e tudo que estiver abaixo dele
 delete carro.condutores
 delete carro.proprietario.endereco
 delete carro.calcularValorSeguro
 console.log(carro)
 console.log(carro.condutores)
-//console.log(carro.condutores.length) // cuidado ao tentar acessar propriedade de algo que esta indefinido.
+// cuidado ao tentar acessar propriedade de algo que esta indefinido, pois pode gerar um erro
+//console.log(carro.condutores.length)
