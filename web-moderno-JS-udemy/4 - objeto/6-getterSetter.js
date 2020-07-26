456dc030e2cf88920fabf877638fb9ed6ed91f86
Brenno Daniel <brenno.daniel.98@gmail.com>
2020-07-25 22:30:51 -0300

    update notes and code example

--- web-moderno-JS-udemy/4 - objeto/6-getterSetter.js
+++ web-moderno-JS-udemy/4 - objeto/6-getterSetter.js
@@ -1,1 +1,16 @@
 // Aprendendo encapsulamento com os métodos Getter e Setter
+const sequencia = {
+    _valor: 1, // convenção
+    get valor() { return this._valor++ },
+    set valor(valor) {
+        if(valor > this._valor) {
+            this._valor = valor
+        }
+    }
+}
+
+console.log(sequencia.valor, sequencia.valor)
+sequencia.valor = 1000
+console.log(sequencia.valor, sequencia.valor)
+sequencia.valor = 900
+console.log(sequencia.valor, sequencia.valor)
