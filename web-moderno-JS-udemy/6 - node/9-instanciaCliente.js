9b06196252b9962325d5835b9e94d842bdc26a0e
Brenno Daniel <brenno.daniel.98@gmail.com>
2020-08-01 22:26:58 -0300

    Introduction to Node.js <3

--- web-moderno-JS-udemy/6 - node/9-instanciaCliente.js
+++ web-moderno-JS-udemy/6 - node/9-instanciaCliente.js
@@ -2,7 +2,7 @@ const contadorA = require('./7-instanciaUnica')
 const contadorB = require('./7-instanciaUnica')
 
 // Evitando o cache do node com uma factory
-// no caso criando novas instâncias de um obj através de uma factory
+// No caso criando novas instâncias de um obj através de uma factory
 const contadorC = require('./8-instanciaNova')() // precisamos retornar o obj ()
 const contadorD = require('./8-instanciaNova')()
 
